1af458afc53af5d2fe3bbcd5a0e4f2283c2b1f7d
sjmitch-git <sjmitch20@outlook.com>
2023-12-15 19:01:47 +0000

    Implements navigation

--- client/src/components/nav.tsx
+++ client/src/components/nav.tsx
@@ -1,3 +1,19 @@
-const Nav: React.FC = () => <nav>!Nav</nav>;
+import { NavLink } from "react-router-dom";
+
+const Nav: React.FC = () => (
+  <nav>
+    <ul className="nav">
+      <li>
+        <NavLink to="/">Home</NavLink>
+      </li>
+      <li>
+        <NavLink to="/confession">Confession</NavLink>
+      </li>
+      <li>
+        <NavLink to="/misdemeanour">Misdemeanour</NavLink>
+      </li>
+    </ul>
+  </nav>
+);
 
 export default Nav;
